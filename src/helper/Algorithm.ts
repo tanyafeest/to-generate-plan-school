73a5d5d522fb6bddf80cc76585748d0298b18ca2
1td <35337541+1td@users.noreply.github.com>
2022-02-03 13:15:30 +0100

    Added neighbour count and the start of its calculation
    also added new list called "have rule" listing all Students affected by a rule.

--- src/helper/Algorithm.ts
+++ src/helper/Algorithm.ts
@@ -3,7 +3,23 @@ import { Sitzplatz } from './Sitzplatz';
 import { Student } from './Student';
 
 export default function compute(sitzplaetze : Sitzplatz[], students : Student[]){
-    let frontSeats : Student[] = [];
+
+    
+
+    let haveRule: Student[] = [];
+    students.forEach(i => {
+        if (i.frontRow) {
+            haveRule.push(i);
+        }
+        else if (i.avoid.length != 0) {
+            haveRule.push(i);
+        }
+        else if (i.sitWith.length != 0) {
+            haveRule.push(i);
+        }
+    });
+
+    let frontSeats: Student[] = [];
     students.forEach(i => {
         if (i.frontRow) {
             frontSeats.push(i);
@@ -12,7 +28,55 @@ export default function compute(sitzplaetze : Sitzplatz[], students : Student[])
 
 }
 
-function matchStudents(l1: Student[], l2: Student[]){
+function calculateNeighbours(sitzplaetze : Sitzplatz[]) {
+    let maxX: number = 0;
+    let maxY: number = 0;
+    sitzplaetze.forEach(i => {
+        if (i.x > maxX) {
+            maxX = i.x;
+        }
+        if (i.y > maxY) {
+            maxY = i.y
+        }
+    });
+
+    let sitzplaetzeR: number[][] = [];
+    for (let i = 0; i <= maxX; i++) {
+        let row: number[] = [];
+        for (let j = 0; j <= maxY; j++) {
+            row.push(-1);
+        }
+        sitzplaetzeR.push(row);
+    }
+
+    sitzplaetze.forEach(i => {
+        for (let j = minLimit(i.x - 1, 0); j <= maxLimit(i.x + 1, maxX); j++) {
+            for (let k = minLimit(i.y - 1, 0); k <= maxLimit(i.y + 1, maxX); k++) {
+                sitzplaetzeR[j][k]++;
+            }
+        }
+    });
+}
+
+function minLimit(input: number, limit: number) {
+    if (input >= limit) {
+        return input;
+    }
+    else {
+        return limit;
+    }
+}
+
+function maxLimit(input: number, limit: number) {
+    if (input <= limit) {
+        return input;
+    }
+    else {
+        return limit;
+    }
+}
+
+function matchLists(l1: Object[], l2: Object[]){
     //takes two lists and returns true if any element of the first is also in the second.
     l1.forEach(i => {
         if (l2.includes(i)) {
