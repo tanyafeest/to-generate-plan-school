943590f60e72bf5573e84df9cc93c65e0689d523
1td <35337541+1td@users.noreply.github.com>
2022-02-15 11:29:30 +0100

    started adding randomness

--- src/helper/Algorithm.ts
+++ src/helper/Algorithm.ts
@@ -2,7 +2,9 @@
 import { Sitzplatz } from './Sitzplatz';
 import { Student } from './Student';
 
-export default function compute(sitzplaetze: Sitzplatz[], students: Student[]) {
+
+
+export default function compute(sitzplaetze: Sitzplatz[], students: Student[], randomness = 0) {
     if (sitzplaetze.length < students.length) {
         alert("Es gibt weniger Plätze als Schüler")
         return;
@@ -23,7 +25,7 @@ export default function compute(sitzplaetze: Sitzplatz[], students: Student[]) {
         }
     });
     
-    calculateNeighbours(sitzplaetze, true);
+    orderbyNeighbours(sitzplaetze, true, 0);
     
     const unsolvedFrontSeats: Sitzplatz[] = [];
     const unsolvedSeats: Sitzplatz[] = [];
@@ -162,10 +164,17 @@ function orderByAffability(students: Student[]) {
         i.calculateAffability();
     });
     students.sort((a, b) => a.affability - b.affability);
-    console.log(students);
 }
 
-function calculateNeighbours(sitzplaetze: Sitzplatz[], calculateRows = false) { // calculates the number of neighbours for every seat and sorts the given array, also calculates front Seats.
+function orderbyNeighbours(sitzplaetze: Sitzplatz[], calculateRows = false, randomness = 0, alwaysCalculateNeighbours = false) {
+    /** calculates the number of neighbours for every seat and sorts the given array, also calculates front Seats.
+     * @param {number} randomness
+     * how random the order will be: 
+     * 0 (or other values under 1) will always return the same order,
+     * 1 (or higher) will  shuffle seats with the same neighbour count and
+     * 2 (or higher) will return seats in a completely random order.
+     * @param {boolean} alwaysCalculateNeighbours
+     * with a randomness of 2 or higher */ 
     let maxX = 0;
     let minY = 100000;
     let maxY = 0;
@@ -181,28 +190,38 @@ function calculateNeighbours(sitzplaetze: Sitzplatz[], calculateRows = false) { 
         }
     })
 
-    const sitzplaetzeR = [] as number[][];
-    for (let i = 0; i <= maxX; i++) {
-        const row = [] as number[];
-        for (let j = 0; j <= maxY; j++) {
-            row.push(-1);
+    if (randomness < 2 || alwaysCalculateNeighbours) {
+        const sitzplaetzeR = [] as number[][];
+        for (let i = 0; i <= maxX; i++) {
+            const row = [] as number[];
+            for (let j = 0; j <= maxY; j++) {
+                row.push(-1);
+            }
+            sitzplaetzeR.push(row);
         }
-        sitzplaetzeR.push(row);
-    }
 
-    sitzplaetze.forEach(i => {
-        for (let j = Math.max(i.x - 1, 0); j <= Math.min(i.x + 1, maxX); j++) {
-            for (let k = Math.max(i.y - 1, 0); k <= Math.min(i.y + 1, maxY); k++) {
-                sitzplaetzeR[j][k]++;
+        sitzplaetze.forEach(i => {
+            for (let j = Math.max(i.x - 1, 0); j <= Math.min(i.x + 1, maxX); j++) {
+                for (let k = Math.max(i.y - 1, 0); k <= Math.min(i.y + 1, maxY); k++) {
+                    sitzplaetzeR[j][k]++;
+                }
+            }
+        })
+
+        sitzplaetze.forEach(i => {
+            i.neighbours = sitzplaetzeR[i.x][i.y];
+        })
+
+        if (randomness < 2) {
+            sitzplaetze.sort((a, b) => a.neighbours - b.neighbours);
+            if (randomness >= 1) {
+                // TODO: shuffle individual neighbour groups
             }
         }
-    })
-
-    sitzplaetze.forEach(i => {
-        i.neighbours = sitzplaetzeR[i.x][i.y];
-    })
-
-    sitzplaetze.sort((a, b) => a.neighbours - b.neighbours);
+    }
+    else {
+        shuffleArray(sitzplaetze);
+    }
 
     console.log(sitzplaetze);
     if (calculateRows) {
@@ -276,7 +295,7 @@ function maxLimit(input: number, limit: number) {
     }
 }
 
-function matchLists(l1: [], l2: []) {
+function matchLists(l1: Object[], l2: Object[]) {
     //takes two lists and returns true if any element of the first is also in the second.
     l1.forEach(i => {
         if (l2.includes(i)) {
@@ -284,4 +303,11 @@ function matchLists(l1: [], l2: []) {
         }
     })
     return false;
+}
+
+function shuffleArray(array: Object[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
